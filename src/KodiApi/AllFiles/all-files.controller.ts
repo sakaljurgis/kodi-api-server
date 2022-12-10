f277d574063916ed0dd018e846ee9b401d37bf54
jurgis <sakaljurgis>
2022-12-10 14:22:25 +0100

    add all-files controller paths

--- src/KodiApi/AllFiles/all-files.controller.ts
+++ src/KodiApi/AllFiles/all-files.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import ApiResponse from '../Dto/api-response.dto';
 import { AllFilesService } from './all-files.service';
 
@@ -9,5 +9,30 @@ export class AllFilesController {
   @Get()
   getMenu(): ApiResponse {
     return this.allFilesService.getMenu();
+  }
+
+  @Get('show')
+  getShows() {
+    return { will: 'show shows list' };
+  }
+
+  @Get('movie')
+  getMovies() {
+    return { will: 'show movies list' };
+  }
+
+  @Get('play')
+  play() {
+    return { will: 'return a playable stream' };
+  }
+
+  @Get(':titleId/:seasonId?')
+  getTitle(
+    @Param('titleId') titleId: number,
+    @Param('seasonId') seasonId: number,
+  ) {
+    return {
+      will: 'show title videos list title ' + titleId + ', season ' + seasonId,
+    };
   }
 }
